0e91203dfdfcad2b783dcb2932015674f7eeb80a
Jim LAURIE <hack@jimlaurie.me>
2014-12-15 09:56:01 +0100

    Initial Commit

---
+++ ios-mob/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-mob
+//
+//  Created by Jim LAURIE on 15/12/2014.
+//  Copyright (c) 2014 hetic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
