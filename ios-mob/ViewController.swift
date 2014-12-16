30a6814eac9df246c8ea63a9d00538b7c975745d
Jim LAURIE <hack@jimlaurie.me>
2014-12-16 10:56:15 +0100

    [Single send] push button -> post message

--- ios-mob/ViewController.swift
+++ ios-mob/ViewController.swift
@@ -7,9 +7,27 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBAction func pushButton(sender: AnyObject) {
+        let apiUrl = "https://slack.com/api/"
+        let token = "xoxp-3221071404-3221071414-3221836016-5659d9"
+        let channel = "#test"
+        var slackParams = [
+            "token": token,
+            "channel": channel,
+            "username": "Jim",
+            "text": "console.log('coucou');"
+        ]
+        Alamofire.request(.POST, apiUrl + "chat.postMessage", parameters: slackParams)
+            .response { (request, response, data, error) in
+                println(request)
+                println(response)
+                println(error)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
