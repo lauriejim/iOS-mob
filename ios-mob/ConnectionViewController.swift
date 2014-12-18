3780e711bd3f04e73220df88c1d68e22b140753c
Kévin <kevinchassagne@gmail.com>
2014-12-18 17:55:29 +0100

    create form nice

---
+++ ios-mob/ConnectionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ConnectionViewController.swift
+//  ios-mob
+//
+//  Created by Kévin Chassagne on 18/12/2014.
+//  Copyright (c) 2014 hetic. All rights reserved.
+//
+
+import UIKit
+
+class ConnectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
